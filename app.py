dc5260fb507eb568d23d45d6cfec0190cf6df77d
ethanoutangoun <ethanoutangoun@gmail.com>
2023-12-05 20:39:14 -0800

    new requirements

--- app.py
+++ app.py
@@ -6,12 +6,7 @@ import base64
 import os
 
 import json
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from bs4 import BeautifulSoup
+
 
 
 app = Flask(__name__)
